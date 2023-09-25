8f26f8c3172f3bb52edc6f8caa752b16b84cc314
Karma10001 <ll.karma.ll1000@gmail.com>
2023-09-25 18:07:25 +0530

    Updated logo and link

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
